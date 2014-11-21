89a92590a92d5e95f233dfa7737861da525622b5
zzk1st <zhu@parth2-e03-40.QHdata.net>
2014-11-21 14:28:48 +0000

    Added pipe support and other functions

--- kvcut.py
+++ kvcut.py
@@ -1,14 +1,37 @@
 import sys, getopt
 
 def usage():
-    print "USAGE: kvcut -f (filename) -k (key1,key2,key3...) -d (delimiter) -v"
-    print "   -v: Print only values"
+    print """kvcut: Cut-style command used to cut key-value style lines
+Usage: python kvcut.py [-f FILE] -k key1,key2... [OPTION]
+Search for keys in each line of the FILE or standard input.
+
+Example: 
+test.txt:
+    a=1|b=2|c=3
+    a=4|b=5
+    c=6
+python kvcut.py -f test.txt -k a,c
+result:
+    a=1 c=3
+    a=4
+    c=6
+
+Regexp selection and interpretation:
+  -f FILENAME           input file
+  -a                    only print when all keys exist in the line
+  -k key1,key2,key3 ... the key(s) to extract
+  -d DELIMITER          the delimiter, by default is "|"
+  -v                    output only the values"""
+  
     return
 
 delimiter="|"
 value_only=False
+only_print_all_keys=False
+print_line_num=False
+filename=""
 
-opts, args = getopt.getopt(sys.argv[1:], "hf:k:d:v", ["help"])
+opts, args = getopt.getopt(sys.argv[1:], "hf:k:d:van", ["help"])
 for opt, arg in opts:
     if opt in ("-h", "--help"):
         usage()
@@ -21,14 +44,23 @@ for opt, arg in opts:
         delimiter=arg
     elif opt == '-v':
         value_only=True
+    elif opt == '-a':
+        only_print_all_keys=True
+    elif opt == '-n':
+        print_line_num=True
 
-f = open(filename)
-lines = f.read().splitlines()
-f.close()
+# read from file or pipe
+if filename == "":
+    lines = sys.stdin.read().splitlines()
+else:
+    f = open(filename)
+    lines = f.read().splitlines()
+    f.close()
 
-sys.stdout.write("keys:")
-print keys
-print "delimiter: " + delimiter
+# print the setting of kvcut
+#sys.stdout.write("keys:")
+#print keys
+#print "delimiter: " + delimiter
 
 line_number = 0
 for line in lines:
@@ -41,6 +73,28 @@ for line in lines:
         value=strs[-1]
         dict[key]=value
     
+    # if in "a" mode, check if all keys exist in the line
+    if only_print_all_keys:
+        all_exist=True
+        for key in keys:
+            if not key in dict.keys():
+                all_exist=False
+        if not all_exist:
+            continue
+
+    # if none of the keys exists in the line, skip the line
+    none_exist=True
+    for key in keys:
+        if key in dict.keys():
+            none_exist=False
+            break;
+    if none_exist:
+        break;
+
+    # print the key and values
+    if print_line_num:
+        sys.stdout.write(str(line_number) + ":")
+
     for key in keys:
         if key in dict.keys():
             if value_only:
